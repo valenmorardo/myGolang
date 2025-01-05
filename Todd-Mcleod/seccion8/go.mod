0525c6e18ad05da1f4a7509a128f443caac02d49
Valentin <valentin.morardo@gmail.com>
2025-01-05 20:49:48 -0300

    module import

---
+++ Todd-Mcleod/seccion8/go.mod
@@ -0,0 +1,5 @@
+module seccion8Module
+
+go 1.23.4
+
+require github.com/valenmorardo/puppy v0.0.0-20250105234301-721569d68e98 // indirect
---
+++ Todd-Mcleod/seccion8/main.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/valenmorardo/puppy"
+)
+
+
+func main() {
+
+	strin1 := puppy.Bark()
+	string2 := puppy.Braks()
+
+	fmt.Println(strin1, string2)
+}
