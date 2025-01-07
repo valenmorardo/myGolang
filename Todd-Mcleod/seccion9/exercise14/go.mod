df2d2f883053b76101dd0d23e30fc84bb7689a31
Valentin <valentin.morardo@gmail.com>
2025-01-06 21:47:27 -0300

    exercise14 done

---
+++ Todd-Mcleod/seccion9/exercise14/go.mod
@@ -0,0 +1,5 @@
+module exercise14
+
+go 1.23.4
+
+require github.com/valenmorardo/puppy v0.0.0-20250105234301-721569d68e98
---
+++ Todd-Mcleod/seccion9/exercise14/main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"fmt"
+	"github.com/valenmorardo/puppy"
+)
+
+func main() {
+	fmt.Println("Hello world, I am Gopher!")
+
+	bark := puppy.Bark()
+
+	fmt.Println(bark)
+}
+
+
+
