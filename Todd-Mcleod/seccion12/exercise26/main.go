53c581229aa6bda268f03e032ba9c108d69032ab
Valentin <valentin.morardo@gmail.com>
2025-01-08 20:45:21 -0300

    exercise26 done: init func

---
+++ Todd-Mcleod/seccion12/exercise26/main.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"fmt"
+	"math/rand"
+)
+
+func init() {
+	fmt.Println("Aca es donde la inicializacion de mi programa comienza!")
+	
+}
+
+
+
+func randomInt(n int) int {
+	return rand.Intn(n)
+}
+
+func main() {
+	x := randomInt(3)
+	fmt.Printf("El valor de la variable x es: %v y es de tipo %T \n", x, x)
+
+	switch {
+	case x <= 100:
+		fmt.Printf("El numero %v de la variable x esta entre 0 y 100", x)
+
+	case x >= 101 && x <= 200:
+		fmt.Printf("El numero %v de la variable x esta entre 101 y 200", x)
+	case x >= 201 && x <= 250:
+		fmt.Printf("El numero %v de la variable x esta entre 201 y 250", x)
+
+	}
+}
