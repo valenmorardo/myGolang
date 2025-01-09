738c68db5431bf648ba9cf42df56fb4ef1f63553
Valentin <valentin.morardo@gmail.com>
2025-01-09 20:54:51 -0300

    exercise37 done

---
+++ Todd-Mcleod/seccion12/exercise37/main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+
+	datos := map[string]int {
+		"James": 42,
+		"Moneypenny": 32,
+	}
+
+	
+
+	
+	if age, ok := datos["James"]; ok {
+		fmt.Printf("La edad es %v", age)
+	} else {
+		fmt.Println("No existe el valor buscado")
+	}
+
+}
