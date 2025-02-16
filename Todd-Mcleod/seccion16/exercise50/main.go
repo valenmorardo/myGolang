9d9e4cf7af9bcb9bbf48e98b3eaf3d75a0f68be5
Valentin Morardo <valentin.morardo@gmail.com>
2025-02-16 20:52:35 -0300

    exercise50 done

---
+++ Todd-Mcleod/seccion16/exercise50/main.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+func main() {
+	personas := make(map[string][]string)
+
+	personas[`bond_james`] = []string{`shaken, no stirred`, `martinis`, `fast cars`}
+	personas[`moneypenny_jenny`] = []string{`intelligence`, `literature`, `computer science`}
+	personas[`no_dr`] = []string{`cats`, `ice cream`, `sunsets`}
+
+	personas[`fleming_ian`] = []string{`steaks`, `cigars`, `espionage`}
+	
+	for key, value := range personas {
+		fmt.Printf("%s %s\n", key, value)
+	}
+
+}
