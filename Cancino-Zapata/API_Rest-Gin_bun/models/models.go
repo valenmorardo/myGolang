b908292610d9bfed2bd7074e6e783d9135d6baeb
valenmorardo <valentin.morardo@gmail.com>
2025-07-23 19:49:26 -0300

    models file TematicasStruct fixed

--- Cancino-Zapata/API_Rest-Gin_bun/models/models.go
+++ Cancino-Zapata/API_Rest-Gin_bun/models/models.go
@@ -7,9 +7,9 @@ import (
 )
 
 type TematicasModel struct {
-	bun.BaseModel `bun:"table:tematicas, alias:t"`
+	bun.BaseModel `bun:"table:tematicas,alias:t"`
 
-	ID     int64  `bun:",pk, autoincrement" json:"id"`
-	Nombre string `bun:"nombre, notnull" json:"nombre"`
-	Slug   string `bun:"slug, notnull" json:"slug"`
+	ID     int64  `bun:",pk,autoincrement" json:"id"`
+	Nombre string `bun:"nombre,notnull" json:"nombre"`
+	Slug   string `bun:"slug,notnull" json:"slug"`
 }
